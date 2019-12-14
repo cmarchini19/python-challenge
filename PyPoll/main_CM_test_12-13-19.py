8e6df60872785b6ceaaa4550ac9775e5d6396993
Chris Marchini <cmarchini@rti.org>
2019-12-14 00:01:35 -0500

    New final PyPoll
    PyPoll sans Winner and text file output

--- PyPoll/main_CM_test_12-13-19.py
+++ PyPoll/main_CM_test_12-13-19.py
@@ -1,4 +1,4 @@
-#PyPoll Analysis
+ #PyPoll Analysis
 #-------------------------------
 
 #Step 1: Import the modules I'll need
@@ -6,16 +6,42 @@ import os
 import csv
 
 #Step 2: Identify the budget_data.csv file we'll use for PyPoll
-bankcsv = os.path.join("Resources","election_data.csv")
+pollcsv = os.path.join("Resources","election_data.csv")
 
-with open(bankcsv, newline="") as csvfile:
-    electionheader = csv.reader(csvfile, delimiter=",")
-    print(electionheader)
-    
-    election_header = next(electionheader)
-    print(f"Header: {election_header}")
-    
-    
-    for row in electionheader:
-        print(sum(row[0])
+totalvotes = 0
+candidates = {}
+percentages = {}
 
+with open(pollcsv, newline="") as csvfile:
+    electionreader = csv.reader(csvfile, delimiter=",")
+    election_reader = next(electionreader)
+    
+    for row in electionreader:
+        #Step 3: Collect number of votes
+        totalvotes = totalvotes + 1
+
+        #Step 4: Identify Candidate Data
+        candidate = row[2]
+
+        #Step 5: how many votes did the candidates receive?
+        if candidate not in candidates.keys():
+            candidates[candidate] = [row[0]]
+            
+        else:
+            candidates[candidate].append(row[0])
+
+    #Step 6: what is the percentage of votes the candidates received?
+    for candidatename in candidates.keys():
+        percentages[candidatename] = len(candidates[candidatename])/totalvotes * 100
+
+#Step 7: Print Values
+print(f'Election Results')
+print("-------------------")
+print(f'Total Votes: {totalvotes}')
+print("-------------------")
+for candidatename in candidates.keys():
+    print(f'{candidatename} {len(candidates[candidatename])} ({round(percentages[candidatename],3)}%)')
+print("-------------------")
+print("Winner")
+
+#Step 8: Write output file/csv
