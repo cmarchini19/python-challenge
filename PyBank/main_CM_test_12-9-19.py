4902191eeba77c56c659e1115edbb9e2201dd591
Chris Marchini <cmarchini@rti.org>
2019-12-13 20:13:37 -0500

    Revised PyBank
    Tried out averages and and greatest increase/decreases. Didn't work...

--- PyBank/main_CM_test_12-9-19.py
+++ PyBank/main_CM_test_12-9-19.py
@@ -6,12 +6,13 @@ import os
 import csv
 
 
-#Step 2: Identify the budget_data.csv file we'll use for PyPoll
+#Step 2: Define and Identify the budget_data.csv file we'll use for PyBank
 bankcsv = os.path.join("Resources","budget_data.csv")
 
 dates = []
 total = 0.0
-average = [(total + int(row[1])]
+#average = [(total + int(row[1])]
+greatestincrease = []
 
 with open(bankcsv, newline="") as csvfile:
     bankreader = csv.reader(csvfile, delimiter=",")
@@ -28,30 +29,27 @@ with open(bankcsv, newline="") as csvfile:
         total = total + int(row[1])
 
         #Step 5: Find the average of the changes in "Profit/Losses" over the entire period (avg. will be -$2,315.12)
-        avg_change = 
+        #avg_change = 
 
-    print(f'Total Months: {len(dates)}')
-    print(f'Total Profit/Loss: ${total}')
+        
+        #Step 6: Find the greatest increase in profits (date and amount) over the entire period (result will be Feb-2012 $1,926,159)
+        
+        #greatestincrease = max(total + int(row[1])
 
+        #Step 7: Find the greatest decrease in losses (date and amount) over the entire period (result will be Sep-2013  $(2,196,167)
+   
 
     
-#Step 4: Find the net total amount of "Profit/Losses" over the entire period (total will be $38,382,578)
-
-    # total = total + int(row[1])
-    # print(f'Total Profit/Loss: {total}')
-
-#print (net)
-
-
-#Step 5: Find the average of the changes in "Profit/Losses" over the entire period (avg. will be -$2,315.12)
-
-
-
-#Step 6: Find the greatest increase in profits (date and amount) over the entire period (result will be Feb-2012 $1,926,159)
+    #Print results
+    
+    print("Financial Analysis")
+    print("------------------")
+    print(f'Total Months: {len(dates)}')
+    print(f'Total Profit/Loss: ${total}')
+    #print(f'Average Profit/Losses Change: ${average})
+    #print(f'Greatest Increase: {max(total)}')
+    #print(f"Greatest Decrease:"  )
 
 
-    #print(f"Greatest Increase:"  )
-
-#Step 7: Find the greatest decrease in losses (date and amount) over the entire period (result will be Sep-2013  $(2,196,167)
 
 #Step 8: Your final script should both print the analysis to the terminal and export a text file with the results.
